fcbfbf2e84668846f8b24cf513e03f1c80f41b4d
Pragna123456 <154363234+Pragna123456@users.noreply.github.com>
2024-02-02 23:11:27 +0530

    Add files via upload

---
+++ RestApiElectricityApplication-45/src/test/java/com/rest/RestApiElectricityApplication45ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiElectricityApplication45ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
